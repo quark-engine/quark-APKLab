ba46ea7ad42ea7f7f6174034844bdc06e6b09f3b
Haeter <cindejze412@gmail.com>
2020-12-06 23:48:32 +0800

    Add Apkinfo class

--- android/apk.py
+++ android/apk.py
@@ -1,25 +1,88 @@
+import os
+import os.path
 import tempfile
 import zipfile
+from functools import cached_property
 
 from .axml import AxmlReader
 
 
-def get_permissions(manifest_file):
-    axml = AxmlReader(manifest_file)
-    permission_list = set()
+class Apkinfo(object):
+    """
+    Information about apk based on radare2 analysis
+    """
 
-    for tag in axml:
-        if 'Name' in tag.keys() \
-                and axml.get_string(tag['Name']) == 'uses-permission':
-            attrs = axml.get_attributes(tag)
+    def __init__(self, apk_file, tmp_dir=None):
+        """
+        Load an apk and extract all the contains to a temporary dictionary.
 
-            if not attrs is None:
-                permission = axml.get_string(attrs[0]['Value'])
-                permission_list.add(permission)
+        :param apk_file: path lead to an apk
+        :type apk_file: str
+        :param tmp_dir: a path where a temporary dictionary will generate, defaults to system temp
+        :type tmp_dir: str, optional
+        """
+        self._filename = os.path.basename(apk_file)
+        self._filepath = apk_file
 
-    return permission_list
+        self._tmp_dir = tempfile.mkdtemp() if tmp_dir is None else tmp_dir
 
+        # Extract file to tmp dir
+        with zipfile.ZipFile(self._filepath) as apk:
+            # Extract manifest
+            apk.extract('AndroidManifest.xml', path=self._tmp_dir)
+            # Extract all dex files
+            self._dex_list = []
+            for dex in filter(lambda f: f.startswith(
+                    'classes') and f.endswith('.dex'), apk.namelist()):
+                apk.extract(dex, path=self._tmp_dir)
+                self._dex_list.append(os.path.join(self._tmp_dir, dex))
 
-def check_valid(file_path):
-    # TODO
-    return True
+    @property
+    def manifest(self):
+        """
+        Return a path to extracted manifest file.
+        """
+        return os.path.join(self._tmp_dir, 'AndroidManifest.xml')
+
+    @property
+    def dex_list(self):
+        """
+        Return a list of paths to extracted dex files
+        """
+        return self._dex_list
+
+    @cached_property
+    def permissions(self):
+        """
+        Return a set of app permissions which was defined in manifest file.s
+        """
+        axml = AxmlReader(self.manifest)
+        permission_list = set()
+
+        for tag in axml:
+            if 'Name' in tag.keys() \
+                    and axml.get_string(tag['Name']) == 'uses-permission':
+                attrs = axml.get_attributes(tag)
+
+                if not attrs is None:
+                    permission = axml.get_string(attrs[0]['Value'])
+                    permission_list.add(permission)
+
+        return permission_list
+
+    def find_method(self, class_name=".*", method_name=".*", descriptor=".*"):
+        # TODO
+        pass
+
+    def check_valid(self):
+        # TODO
+        return True
+
+    def __del__(self):
+        """
+        Clean up all the extracted files.
+        """
+        for dirpath, dirs, files in os.walk(self._tmp_dir, False):
+            for filename in files:
+                os.remove(os.path.join(dirpath, filename))
+        os.rmdir(self._tmp_dir)
--- tests/test_apk.py
+++ tests/test_apk.py
@@ -1,25 +1,57 @@
+import hashlib
+import os.path
+
 import pytest
-import zipfile
-import os
-
-from android.apk import get_permissions
+from android.apk import Apkinfo
 
 
-@pytest.fixture(scope="module")
-def manifest_file():
-    return 'tests/sample/HippoSMS/AndroidManifest.axml'
+@pytest.fixture(scope="class")
+def apkinfo_obj():
+    apk_file = 'tests/sample/HippoSMS.apk'
+    return Apkinfo(apk_file)
 
 
-def test_get_permissions(manifest_file):
-    assert get_permissions(manifest_file) == set([
-        'android.permission.INTERNET',
-        'android.permission.ACCESS_NETWORK_STATE',
-        'android.permission.MOUNT_UNMOUNT_FILESYSTEMS',
-        'android.permission.SEND_SMS',
-        'android.permission.WRITE_EXTERNAL_STORAGE',
-        'android.permission.RECEIVE_BOOT_COMPLETED',
-        'android.permission.RECEIVE_SMS',
-        'android.permission.WRITE_SMS',
-        'android.permission.READ_SMS',
-        'com.android.launcher.permission.INSTALL_SHORTCUT'
-    ])
+def get_md5(file_path):
+    with open(file_path, 'rb') as file:
+        m = hashlib.md5()
+        for chunk in iter(lambda: file.read(4096), b""):
+            m.update(chunk)
+    return m.hexdigest()
+
+
+class TestApkinfo(object):
+
+    def test_manifest(self, apkinfo_obj):
+        md5 = get_md5(apkinfo_obj.manifest)
+        assert md5 == 'af294c67c4d37c27a84b69dbc930efd4'
+
+    def test_dex_list(self, apkinfo_obj):
+        # TODO - Find a sample with multi-dex
+        truth = {
+            'classes.dex': 'ca8bcc8db3963fd1935b21009bcaa717'
+        }
+
+        for dex in apkinfo_obj.dex_list:
+            file_name = os.path.basename(dex)
+            assert file_name in truth.keys()
+            md5 = get_md5(dex)
+            assert md5 == truth[file_name]
+
+    def test_permissions(self, apkinfo_obj):
+        assert apkinfo_obj.permissions == set([
+            'android.permission.INTERNET',
+            'android.permission.ACCESS_NETWORK_STATE',
+            'android.permission.MOUNT_UNMOUNT_FILESYSTEMS',
+            'android.permission.SEND_SMS',
+            'android.permission.WRITE_EXTERNAL_STORAGE',
+            'android.permission.RECEIVE_BOOT_COMPLETED',
+            'android.permission.RECEIVE_SMS',
+            'android.permission.WRITE_SMS',
+            'android.permission.READ_SMS',
+            'com.android.launcher.permission.INSTALL_SHORTCUT'
+        ])
+
+    def test_delete(self, apkinfo_obj):
+        tmp = apkinfo_obj._tmp_dir
+        apkinfo_obj.__del__()
+        assert not os.path.exists(tmp)
