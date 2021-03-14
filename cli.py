6b847ebd0610d0415419514d848fde966297c1ad
haeter525 <cindejze412@gmail.com>
2021-03-14 23:21:37 +0800

    Add thershold on cli

--- cli.py
+++ cli.py
@@ -1,4 +1,3 @@
-from quark.core.analysis import QuarkAnalysis
 import click
 import logging
 import os
@@ -23,10 +22,17 @@ def get_rules_from_directory(dir_path):
               type=click.Path(file_okay=True, dir_okay=False, readable=True, exists=True))
 @click.option('-r', '--rule', help='Json Rule',
               type=click.Path(file_okay=False, dir_okay=True, readable=True, exists=True))
-def main(apk, rule):
-    logging.basicConfig(level=logging.INFO )
-    logging.info(f'Apk File: {apk}')
-    logging.info(f'Rule Directory: {rule}', )
+@click.option('-t', '--thershold', help='Filter for displaying crimes', type=click.IntRange(0, 100), required=False)
+def main(apk, rule, thershold):
+    logging.basicConfig(level=logging.WARN )
+    print(f'Apk File: {apk}')
+    print(f'Rule Directory: {rule}', )
+
+    if thershold:
+        print(f'Thershold:{thershold}%')
+
+    counter = 0
+    displayed = 0
 
     quark = Quark(apk)
 
@@ -34,9 +40,16 @@ def main(apk, rule):
         rule = QuarkRule(rule_file)
         #logging.info(f'Analysing {rule.crime}')
         quark.analysis_rule(rule)
+        counter = counter + 1
 
         max_stage = max([behavior.reached_stage for behavior in quark.report.passed_behaviors[rule]])
-        logging.info(f'Crime @ {rule.crime:<80} {max_stage*20:>3}%')
+        if thershold and max_stage*20 < thershold:
+            continue
+
+        print(f'Crime @ {rule.crime:<80} {max_stage*20:>3}%')
+        displayed = displayed + 1
+        
+    print(f'Total: {displayed}/{counter}')
                 
 
 if __name__ == '__main__':
